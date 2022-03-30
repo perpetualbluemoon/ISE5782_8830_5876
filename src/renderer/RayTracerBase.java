1fa8790b6d661204ba1c999c45215e2db2e16d7b
perpetualbluemoon <nechamaeri@gmail.com>
2022-03-30 17:45:36 +0300

    Stage 5: parts 4 RayTracerBase and RayTracerBasic

---
+++ src/renderer/RayTracerBase.java
@@ -0,0 +1,26 @@
+package renderer;
+
+import primitives.Color;
+import primitives.Ray;
+import scene.Scene;
+
+public abstract class RayTracerBase{
+    protected Scene _scene;
+
+    /***
+     * Constructor
+     * @param scene creates scene
+     */
+    public RayTracerBase(Scene scene){
+        _scene = scene;
+    }
+
+    /***
+     *
+     * @param ray ray through the viewplane
+     * @return color of the point
+     */
+    public abstract Color traceRay(Ray ray);
+
+
+}
--- unittests/renderer/RenderTests.java
+++ unittests/renderer/RenderTests.java
@@ -3,7 +3,7 @@ package unittests.renderer;
 import elements.AmbientLight;
 import org.junit.jupiter.api.Test;
 
-import lighting.AmbientLight;
+//import lighting.AmbientLight;
 import geometries.*;
 import primitives.*;
 import renderer.*;
@@ -26,7 +26,7 @@ public class RenderTests {
                         new Double3(1,1,1))) //
                 .setBackground(new Color(75, 127, 90));
 
-        scene.geometries.add(new Sphere(50, new Point(0, 0, -100)),
+        scene._geometries.add(new Sphere(new Point(0, 0, -100),50),
                 new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
                 // left
                 new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100)), // down
@@ -39,25 +39,6 @@ public class RenderTests {
                 .setImageWriter(new ImageWriter("base render test", 1000, 1000))
                 .setRayTracer(new RayTracerBasic(scene));
 
-        camera.renderImage();
-        camera.printGrid(100, new Color(java.awt.Color.YELLOW));
-        camera.writeToImage();
-    }
-
-    /**
-     * Test for XML based scene - for bonus
-     */
-    @Test
-    public void basicRenderXml() {
-        Scene scene = new Scene("XML Test scene");
-        // enter XML file name and parse from XML file into scene object
-        // ...
-
-        Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-                .setVPDistance(100) //
-                .setVPSize(500, 500)
-                .setImageWriter(new ImageWriter("xml render test", 1000, 1000))
-                .setRayTracer(new RayTracerBasic(scene));
         camera.renderImage();
         camera.printGrid(100, new Color(java.awt.Color.YELLOW));
         camera.writeToImage();
